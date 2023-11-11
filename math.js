0fdc70547d591cda72efb17a6a25ce57d017ae66
amnesiaras <maruko.kc15@gmail.com>
2023-11-11 11:02:52 +0700

    Add more

--- math.js
+++ math.js
@@ -1,8 +1,10 @@
+// 📄 math.js
+
 function sum(a, b) {
-    return a + b
-  }
-  
-  module.exports = {
-    sum,
+    if (Array.isArray(a)) {
+      return a.reduce((acc, n) => acc + n, 0)
+    } else if (typeof a === 'number' && typeof b === 'number') {
+      return a + b
+    }
   }
   
